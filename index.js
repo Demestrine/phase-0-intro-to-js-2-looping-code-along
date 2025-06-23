d0ee647202775be227705c467b90aff196d49576
Demestrine <demestrinea@gmail.com>
2025-06-23 08:53:01 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  const thankYouCards = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards;
+}
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+// For testing purposes
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = {
+    writeCards,
+    countDown
+  };
+}
